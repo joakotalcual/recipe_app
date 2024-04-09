38fa29e32b0eb845492750d6473759bc2cecc831
joakotalcual <esparza100401@gmail.com>
2024-04-08 18:46:56 -0600

    commit

--- lib/screens/profile/components/body.dart
+++ lib/screens/profile/components/body.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: defaultSize * 24, //240
+          child: Stack(
+            children: <Widget>[
+              ClipPath(
+                child: Container(
+                  child: defaultSize * 15, //150
+                  color: kPrimaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+class CustomShape extends CustmClipper<Path>{
+  @override
+  Path getClip(Size size){
+    throw 
   }
 }
